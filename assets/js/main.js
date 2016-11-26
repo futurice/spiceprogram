c8b34e2b70e3db455e81f613edf5d1468415bcec
Antti Mattila <amat@fl960.local>
2016-11-26 17:52:05 +0200

    Add clouds and animate them

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,40 @@ $(function() {
     $('#shareThisLink').on('mouseout', function() {
         $(this).find('span').remove();
     });
+
+    doClouds();
 });
+
+var doClouds = function() {
+    var screenWidth = window.screen.width
+    var clouds = document.querySelectorAll('.cloud');
+    var animatedClouds = [];
+
+    var cloud = function (cloudEl) {
+        var xPos = Math.random() * screenWidth;
+        var speed = Math.random();
+
+        this.animate = function() {
+            cloudEl.style.transform = 'translateX(' + (xPos += speed) + 'px)'; 
+            if (xPos > screenWidth + 200) {
+                xPos = -200;
+            }
+        };
+    };
+
+    var createClouds = function() {
+        return Array.prototype.map.call(clouds, function(c) {
+            return new cloud(c, screenWidth);
+        });
+    };
+
+    var render = function() {
+        animatedClouds.forEach(function(c) {
+            c.animate();
+        })
+        requestAnimationFrame(render);
+    };
+
+    animatedClouds = createClouds();
+    render();
+}
